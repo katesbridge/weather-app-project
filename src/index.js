4816a7e44a93b1ed3fa07e28fa68174e7f98531b
katesbridge <83126457+katesbridge@users.noreply.github.com>
2023-01-12 01:24:06 +0200

    test https

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ function displayForecast() {
       <div class="col-2">
         <div class="weather-forecast-date">${day}</div>
         <img
-          src="http://openweathermap.org/img/wn/02d@2x.png"
+          src="https://openweathermap.org/img/wn/02d@2x.png"
           alt=""
           width="42"
         />

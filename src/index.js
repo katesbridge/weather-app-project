f9bac90145c1d2e5efcbe730031f40c629db5ec1
katesbridge <83126457+katesbridge@users.noreply.github.com>
2023-01-12 00:39:32 +0200

    forecast JS template added

--- src/index.js
+++ src/index.js
@@ -47,6 +47,32 @@ function displayWeatherCondition(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   IconElement.setAttribute("alt", response.data.weather[0].description);
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/02d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 1° </span>
+          <span class="weather-forecast-temperature-min"> -7° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
 }
 
 function searchCity(city) {
